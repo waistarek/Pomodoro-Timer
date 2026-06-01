93c9203edd636e0084956c0a0525ced1071df4ec
Tarek Wais <tarek.wais@student.hs-rm.de>
2026-06-01 17:45:20 +0200

    mitwidth of the navigationrail added

--- frontend/flutter_app/lib/screens/app_shell.dart
+++ frontend/flutter_app/lib/screens/app_shell.dart
@@ -162,6 +162,7 @@ class _AppShellState extends State<AppShell> {
               selectedIndex: _index,
               onDestinationSelected: _selectIndex,
               labelType: NavigationRailLabelType.all,
+              minWidth:112,
               destinations: const [
                 NavigationRailDestination(
                   icon: Icon(Icons.timer_outlined),
