0590693407ca6784c037494c43ee02a24d4a7c0c
Tarek Wais <tarek.wais@student.hs-rm.de>
2026-06-08 21:47:59 +0200

    Fix Flutter web path routing for go_router

--- frontend/flutter_app/lib/main.dart
+++ frontend/flutter_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-
+import 'services/url_strategy_service.dart';
 import 'l10n/app_localizations.dart';
 import 'providers/auth_provider.dart';
 import 'providers/session_sync_provider.dart';
@@ -23,6 +23,7 @@ import 'router/app_router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  configureUrlStrategy();
 
   final localStorage = LocalStorageService();
   await localStorage.init();
---
+++ frontend/flutter_app/lib/services/url_strategy_service.dart
@@ -0,0 +1,2 @@
+export 'url_strategy_service_stub.dart'
+    if (dart.library.html) 'url_strategy_service_web.dart';
